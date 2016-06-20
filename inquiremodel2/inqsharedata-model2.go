5c67af5648326c2acee64fe5d31738321728cad7
sriefx143 <sri.krishnamacharya@equifax.com>
2016-06-20 13:34:14 -0400

    test build 5

--- inquiremodel2/inqsharedata-model2.go
+++ inquiremodel2/inqsharedata-model2.go
@@ -184,7 +184,7 @@ func (t *ShareInfoCode) write(stub *shim.ChaincodeStub, args []string) ([]byte, 
 	bytestostore, _ := json.Marshal(res1)
 
 	bytesofdata, _ := stub.GetState(user + "-shareinfo")
-	var newItemsString string
+	var newItemsString string = ""
 	if len(args[4]) != 0 {
 		var storedString string = string(bytestostore)
 		var arrStored []string = strings.Split(storedString, "^")
@@ -205,10 +205,10 @@ func (t *ShareInfoCode) write(stub *shim.ChaincodeStub, args []string) ([]byte, 
 				}
 
 			}
-			if args[4] == "U" {
-				newItemsString = newItemsString + "^" + string(bytestostore)
-			}
 
+		}
+		if args[4] == "U" {
+			newItemsString = newItemsString + "^" + string(bytestostore)
 		}
 		_ = stub.PutState(user+"-shareinfo", []byte(newItemsString))
 
